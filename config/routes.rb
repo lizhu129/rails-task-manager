f755109934eaa77863638b4b3ef7aacb3b9b7754
Li Zhu <lizhu129@gmail.com>
2023-07-25 11:04:11 +0200

    add all CRUD operations

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # List
+  get 'tasks', to: 'tasks#index'
+  # Create
+  get 'tasks/new', to: 'tasks#new', as: :new_task
+  post 'tasks', to: 'tasks#create'
+  # Read
+  get 'tasks/:id', to: 'tasks#show', as: :task
+  # Update
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch 'tasks/:id', to: 'tasks#update'
+  # Delete
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
