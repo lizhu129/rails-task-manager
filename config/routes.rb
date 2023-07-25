1d992d51e66d3dbd88a9a1b38205ba6885e08177
Li Zhu <lizhu129@gmail.com>
2023-07-25 11:24:04 +0200

    refactor code

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,5 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # List
-  get 'tasks', to: 'tasks#index'
-  # Create
-  get 'tasks/new', to: 'tasks#new', as: :new_task
-  post 'tasks', to: 'tasks#create'
-  # Read
-  get 'tasks/:id', to: 'tasks#show', as: :task
-  # Update
-  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
-  patch 'tasks/:id', to: 'tasks#update'
-  # Delete
-  delete 'tasks/:id', to: 'tasks#destroy'
+  resources :tasks
 end
